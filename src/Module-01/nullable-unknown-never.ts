353a8c075bb78bb061498feea76b10f91dc9b8b2
Md Sakib Mia <sakib01181@gmail.com>
2025-07-08 20:10:28 +0600

    unknown never type

--- src/Module-01/nullable-unknown-never.ts
+++ src/Module-01/nullable-unknown-never.ts
@@ -1,3 +1,6 @@
+
+// Nullable, Unknown, and Never Types in TypeScript
+// Nullable type allows a variable to be null or undefined
 const searchName = (value: string | null) => {
     if (value === null) {
         return console.log("Nothing Here to see");
@@ -6,4 +9,21 @@ const searchName = (value: string | null) => {
         return console.log("searching for " + value);
     }
 }
-searchName(null);
+// searchName(null);
+const myCarSpeedTest = (speed: unknown) => {
+    if (typeof speed === 'number') {
+        const converter = speed * 1000 / 3600
+        console.log(`My Car Speed Is ${converter}`);
+    } else if (typeof speed === 'string') {
+        const value = speed.split(" ")
+        // console.log(value);
+        const converter = parseFloat(value[0]) * 1000 / 3600
+        console.log(`My Car Speed Is ${converter}`);
+    }else {
+        console.log('Your Input Is Wrong Type');
+    }
+}
+
+myCarSpeedTest(40)
+myCarSpeedTest('40 kmh')
+myCarSpeedTest(null)
