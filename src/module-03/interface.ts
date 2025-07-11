2410e7c58cb503461e5c35aead236234189514f0
Md Sakib Mia <sakib01181@gmail.com>
2025-07-11 15:57:46 +0600

    interface practice

---
+++ src/module-03/interface.ts
@@ -0,0 +1,11 @@
+interface GirlFriendList {
+    name: string,
+    age: number,
+    height: number
+}
+
+const GF1: GirlFriendList = {
+    name : 'Laky Khatun',
+    age : 22,
+    height : 5.4
+}
