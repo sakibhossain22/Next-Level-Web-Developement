81d669f94ff58624c3c45f3a409492da39f9e2be
Md Sakib Mia <sakib01181@gmail.com>
2025-07-19 09:04:44 +0600

    inheritence of opp

--- src/module-3/inheritence-opp.ts
+++ src/module-3/inheritence-opp.ts
@@ -1,1 +1,35 @@
-class Parent 
+class Parent {
+    name: string
+    age: number
+    address: string
+
+    constructor(name: string, age: number, address: string) {
+        this.name = name
+        this.age = age
+        this.address = address
+    }
+    sleepTime(sleepHour: number) {
+        return `The ${this.name} sleep ${sleepHour} hours in a day`
+    }
+}
+
+
+
+class Student extends Parent {
+    constructor(name: string, age: number, address: string) {
+        super(name, age, address)
+    }
+}
+
+const student = new Student('Shakib Hossain', 25, 'Sherpur')
+
+
+// class of teacher
+class Teacher extends Parent {
+    designation: string
+
+    constructor(name: string, age: number, address: string, designation: string) {
+        super(name, age, address)
+        this.designation = designation
+    }
+}
